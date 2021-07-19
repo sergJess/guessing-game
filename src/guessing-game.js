fd2c3c1406fbe2e0bba796692e51de44d301a865
sergJess <serg.fantasy@yandex.ru>
2021-07-19 20:27:11 +0300

    feat: add solution for task guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.value = 0;
+        this.min = 0;
+        this.max = 0
+    }
 
     setRange(min, max) {
-
+this.min = min;
+this.max = max;
     }
 
     guess() {
-
+this.value = Math.round((this.min + this.max) / 2); 
+return this.value;
     }
 
     lower() {
-
+        this.max = this.value ;
     }
 
     greater() {
 
+this.min =  this.value ;
     }
 }
 
